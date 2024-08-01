6875bbdc52935ff6c9bc0a38dce0fb800b2b3e9f
GeorgeTatge <georgetatge1@gmail.com>
2024-08-01 17:11:50 -0400

    inputting my own stuff

--- personal-website/gatsby-config.js
+++ personal-website/gatsby-config.js
@@ -9,12 +9,12 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `George Tatge's Website`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `George Tatge`,
+      summary: `who lives and works in Marietta GA building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A website showcasing all the things I have worked on.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
       twitter: `kylemathews`,
@@ -110,15 +110,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `George Tatge's WEbsite`,
+        short_name: `GeorgeTatge`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Website_Logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
